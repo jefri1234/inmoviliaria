cb6ae0845c1e782ddddab837ef8c520a715796c0
BillAndyQ <billandyqm@gmail.com>
2024-07-12 18:24:08 -0500

    andy

--- app/api/usuario/route.js
+++ app/api/usuario/route.js
@@ -3,9 +3,8 @@ import { NextResponse } from "next/server";
 import {prisma} from '@/app/libs/prisma'
 
 export async function GET(){
-
     const dataTodo= await prisma.usuario.findMany()
-
+    
     return NextResponse.json({
         "message":"registro encontrado",
         "status":200,
